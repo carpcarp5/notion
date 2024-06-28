7f7e43c4a81779a021572a3b09b519595e713542
carpcarp5 <167047048+carpcarp5@users.noreply.github.com>
2024-06-28 18:01:32 +0900

    to sidebar 2:21:16

--- notion-clone/app/(marketing)/_components/navbar.tsx
+++ notion-clone/app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,14 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner"
+import { Link } from "lucide-react";
 
 export const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     const scrolled = useScrollTop();
 
     return (
@@ -15,6 +21,30 @@ export const Navbar = () => {
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get Angtion free
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/document">
+                                Enter Angtion
+                            </Link>
+                        </Button>
+                        <UserButton 
+                            afterSignOutUrl="/"
+                        />
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
---
+++ notion-clone/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default{
+    providers: [
+        {
+            domain: "https://superb-kite-64.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
