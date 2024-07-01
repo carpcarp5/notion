2a4db668176954cad86c7fd8a0dd74a95f1708a3
carpcarp5 <167047048+carpcarp5@users.noreply.github.com>
2024-07-01 11:44:58 +0900

    schema & api

--- notion-clone/app/layout.tsx
+++ notion-clone/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/prioviders/theme-provider";
 import { ConvexClientProvider } from "@/components/prioviders/convex-providers";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,6 +42,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="Angtion-theme-2"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
